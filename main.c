db52988c23f58f138d7104e555280ae10a683d28
YannyLiu <liuyang20010113@163.com>
2019-12-12 11:28:03 +0800

    添加注释

--- main.c
+++ main.c
@@ -15,23 +15,25 @@ int main(void)
     char *temp;
     unsigned long long length, start;
     int flag = 0;
-    if(strlen(a) > strlen(b))
+    if(strlen(a) > strlen(b))                               /*使字符串a始终为两个字符串中较短的一个*/
     {
-        temp = (char *)malloc(sizeof(a));
+        temp = (char *)malloc(sizeof(a));                   /*若a长度大于b长度，二者交换*/
         strcpy(temp, a);
         strcpy(a, b);
         strcpy(b, temp);
+        free(temp);
     }
-    for(length = strlen(a);length >= 2;length--)
+    for(length = strlen(a);length >= 2;length--)            /*子串长度，从字符串长度到2递减*/
     {
-        for(start = 0;start <= strlen(a) - length;start++)
+        for(start = 0;start <= strlen(a) - length;start++)  /*子串开始处，用数组方法表示*/
         {
             temp = get_str(a + start, (int)length);
-            if(strstr(b, temp) != NULL)
+            if(strstr(b, temp) != NULL)                     /*若子串再字符串b中存在，则打印*/
             {
                 printf("%s\n", temp);
-                flag = 1;
+                flag = 1;                                   /*有打印的字符串使，flag设为1*/
             }
+            free(temp);
         }
         if(flag)
         {
@@ -42,6 +44,10 @@ int main(void)
 }
 
 char * get_str(const char *start, int length)
+/*get_str() 函数：将从指针start开始的length个字符提取出来形成一个字符串，返回这个字符串的地址
+参数列表：
+ const char *start:提取字符串的开始地址
+ int length:提取字符串的长度*/
 {
     char *out = (char *)malloc((length + 1) * sizeof(char));
     char *b = out;
